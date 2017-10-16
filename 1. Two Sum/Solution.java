7b65842a4c61e52b64f6568f030295f95e13a5fa
ding327 <ding327@users.noreply.github.com>
2017-10-16 09:24:42 +0800

    Update Solution.java

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -1,6 +1,6 @@
 
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -13,5 +13,11 @@ public class Solution {
             map.put(nums[i], i + 1);
         }
         return result;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] ints = twoSum(nums, 9);
+        System.out.println("结果是：" + ints[0] + "\t" + ints[1]);
     }
 }
